c2341bd131598684c94ec13c061962b17d2ef253
hankel343 <57358008+hankel343@users.noreply.github.com>
2023-07-26 22:58:16 -0500

    1.12: anecdotes step1

--- part_2/section_d_exercises/unicafe_step1/src/App.js
+++ part_2/section_d_exercises/unicafe_step1/src/App.js
@@ -1,96 +1,59 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
-  const good = props.good;
-  const neutral = props.neutral;
-  const bad = props.bad;
-  const all = good + neutral + bad;
-
-  const outputHtml = (hasFeedback) => {
-    if (!hasFeedback) {
-      return (
-        <>
-        <p>No feedback provided</p>
-        </>
-      )
-    } else {
-      return (
-        <>
-        <table>
-          <thead>
-            {/* .. */}
-          </thead>
-          <tbody>
-            <StatisticsLine text={"good "} value={good}/>
-            <StatisticsLine text={"neutral "} value={neutral}/>
-            <StatisticsLine text={"bad "} value={bad}/>
-            <StatisticsLine text={"average"} value={((good + -bad) / all).toFixed(1)}/>
-            <StatisticsLine text={"positive"} value={good / all * 100 + '%'}/>
-          </tbody>
-        </table>
-        </>
-      )
-    }
-  }
+const Button = (props) => {
+  const text = props.text;
+  const handler = props.handler;
 
   return (
     <>
-      { outputHtml(all > 0) }
+      <button onClick={handler(props.i + 1)}>
+        {text}
+      </button>
     </>
   )
 }
 
-const StatisticsLine = (props) => {
-  const text = props.text;
-  const value = props.value;
+const Display = (props) => {
+  const i = props.i;
+  const anecdotes = props.anecdotes;
 
   return (
     <>
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
+      <p>{anecdotes[i]}</p>
     </>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const handleGood = (val) => () => {
-    console.log('good now:', val);
-    setGood(val);
-  }
+  const advanceIterator = (idx) => () => {
+    console.log("selected is: ", idx);
+    setSelected(idx);
 
-  const handleNeutral = (val) => () => {
-    console.log('neutral now:', val);
-    setNeutral(val);
-  }
-
-  const handleBad = (val) => () => {
-    console.log('bad now:', val);
-    setBad(val);
+    if (idx === anecdotes.length) {
+      setSelected(-1);
+    } else {
+      setSelected(idx);
+    }
   }
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={handleGood(good + 1)}>
-        good
-      </button>
-
-      <button onClick={handleNeutral(neutral + 1)}>
-        neutral
-      </button>
-
-      <button onClick={handleBad(bad + 1)}>
-        bad
-      </button>
-
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Button text={"next anecdote"} handler={advanceIterator} i={selected}/>
+      <Display i={selected} anecdotes={anecdotes}/>
+      {/* {anecdotes[selected]} */}
     </div>
   )
 }
