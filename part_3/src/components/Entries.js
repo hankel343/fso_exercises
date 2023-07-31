c5139485579159329f6a8ce4639d3e4787e66792
hhaldin <hankel343@gmail.com>
2023-07-30 20:34:19 -0500

    2.14: not complete

--- part_3/src/components/Entries.js
+++ part_3/src/components/Entries.js
@@ -1,18 +1,28 @@
-const Entries = ({ persons, filterStr }) => {
-  if (filterStr === '') {
-    return persons.map((person) => (
-      <div key={person.id}>
-        <p>{person.name} {person.number}</p>
-      </div>
-    ));
-  }
+import personService from '../services/addperson';
+import Button from './Button'
 
-  const filteredPersons = persons.filter(person => person.name.includes(filterStr));
-  return filteredPersons.map((person) => (
-    <div key={person.id}>
-      <p>{person.name} {person.number}</p>
-    </div>
-  ));
+const Entries = ({ persons, filterStr }) => {
+
+    const deleteEntry = (person) => {
+        personService
+            .getAll()
+    }
+
+    if (filterStr === '') {
+        return persons.map((person) => (
+            <div key={person.id}>
+                <p>{person.name} {person.number}</p>
+            </div>
+        ));
+    }
+
+    const filteredPersons = persons.filter(person => person.name.includes(filterStr));
+    return filteredPersons.map((person) => (
+        <div key={person.id}>
+            <p>{person.name} {person.number}</p>
+            <Button text={'Delete'} clickHandler={deleteEntry} />
+        </div>
+    ));
 };
 
 export default Entries
